e8a6156e3bcf99790fcdf1f331cd89e3848ab610
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-04-30 19:23:20 +0300

    clean

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,29 +1,7 @@
-"use client";
+import React from "react";
 
-import { Amplify } from "aws-amplify";
-import awsExports from "../../aws-exports";
-import { Authenticator } from "@aws-amplify/ui-react";
+const page = () => {
+  return <div>page</div>;
+};
 
-import "@aws-amplify/ui-react/styles.css";
-// import "../styles/globals.css";
-
-Amplify.configure({ ...awsExports });
-
-export default function Home() {
-  console.log("2020", 2020);
-  return (
-    <Authenticator>
-      {({ signOut, user }) => {
-        debugger;
-
-        console.log("user", user);
-        return (
-          <main>
-            <h1>Hello, {user?.username}!</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        );
-      }}
-    </Authenticator>
-  );
-}
+export default page;
