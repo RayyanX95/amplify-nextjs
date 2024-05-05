eac75d901558d0efa0e3e8f8671023b16f5b764b
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-05-05 21:44:04 +0300

    Integrate with dev-users-pool ir

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import { Amplify } from "aws-amplify";
-import awsExports from "../../aws-exports";
-import { Authenticator } from "@aws-amplify/ui-react";
+// import { Amplify } from "aws-amplify";
+// import awsExports from "../../aws-exports";
+// import { Authenticator } from "@aws-amplify/ui-react";
 
-import "@aws-amplify/ui-react/styles.css";
-// import "../styles/globals.css";
+// import "@aws-amplify/ui-react/styles.css";
+// // import "../styles/globals.css";
 
-Amplify.configure({ ...awsExports });
+// Amplify.configure({ ...awsExports });
 
-export default function Home() {
-  console.log("2020", 2020);
-  return (
-    <Authenticator>
-      {({ signOut, user }) => {
-        debugger;
+// export default function Home() {
+//   console.log("2020", 2020);
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => {
+//         debugger;
 
-        console.log("user", user);
-        return (
-          <main>
-            <h1>Hello, {user?.username}!</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        );
-      }}
-    </Authenticator>
-  );
-}
+//         console.log("user", user);
+//         return (
+//           <main>
+//             <h1>Hello, {user?.username}!</h1>
+//             <button onClick={signOut}>Sign out</button>
+//           </main>
+//         );
+//       }}
+//     </Authenticator>
+//   );
+// }
