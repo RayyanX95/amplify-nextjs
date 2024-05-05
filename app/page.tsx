396243465e44a1bb4cb8438d9061c5529f1f5e7c
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-05-05 21:57:48 +0300

    Integrate with dev-users-pool ir 002

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import awsExports from "../aws-exports";
-import { Authenticator } from "@aws-amplify/ui-react";
 
 import "@aws-amplify/ui-react/styles.css";
 // import "../styles/globals.css";
